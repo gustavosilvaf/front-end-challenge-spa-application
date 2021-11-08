b28b5d7ab0f0003e28aa0ea9634d9db04b449329
gustavosilvaf <gustavofsilva@email.com>
2021-11-08 17:08:36 -0300

    Fix responsive body

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,32 +3,39 @@ import { Header } from "../components/header/header";
 import Image from "next/image";
 import FirstCallImage from "../assets/first-call-image.png";
 import { Footer } from "../components/footer/footer";
+import FadeIn from "react-fade-in";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <div className={styles.firstCall}>
-        <div className={styles.limiter}>
-          <div>
-            <h1>Measuring everithing</h1>
-          </div>
-          <div>
-            <Image alt={"measuring tape"} src={FirstCallImage} />
+      <FadeIn>
+        <div className={styles.firstCall}>
+          <div className={styles.limiter}>
+            <div>
+              <h1>Measuring everithing</h1>
+            </div>
+            <div>
+              <Image alt={"measuring tape"} src={FirstCallImage} />
+            </div>
           </div>
         </div>
-      </div>
+      </FadeIn>
 
-      <div className={styles.aboutUs}>
-        <div className={styles.limiter}>
-          <div className={styles.aboutUsContent}>
-            <h3>We are Leader in Measure Tapes</h3>
-            <h5>There are 5x circumference of planet earth in metric tapes.</h5>
+      <FadeIn>
+        <div className={styles.aboutUs}>
+          <div className={styles.limiter}>
+            <div className={styles.aboutUsContent}>
+              <h3>We are Leader in Measure Tapes</h3>
+              <h5>
+                There are 5x circumference of planet earth in metric tapes.
+              </h5>
+            </div>
           </div>
+          <div className={styles.aboutUsBackground}></div>
         </div>
-        <div className={styles.aboutUsBackground}></div>
-      </div>
-      <Footer />
+        <Footer />
+      </FadeIn>
     </>
   );
 }
